d36046a3c9018114c15f0c5f4cf01978cdf48495
harazono <harazono_yoritaka_17@stu-cbms.k.u-tokyo.ac.jp>
2019-08-05 17:23:15 +0900

    WIP

---
+++ naos.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <getopt.h>
+#include <math.h>
+#include <omp.h>
+#include "kmer_library.h"
+#include "cpas_debug.h"
+/*
+#include "htslib/sam.h"
+*/
+
+
+//parse alignments
+
+
+
+
+//detect candidates
+
+//curating candidates
+
+//output results
+
+
+
+int main(){
+  return 0;
+}
