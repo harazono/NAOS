c87b4f1a3be5fd72351b561021b43e666b9ed8fc
harazono <harazono_yoritaka_17@stu-cbms.k.u-tokyo.ac.jp>
2019-08-06 16:43:51 +0900

    WIP

--- naos.cpp
+++ naos.cpp
@@ -6,26 +6,148 @@
 #include <getopt.h>
 #include <math.h>
 #include <omp.h>
-#include "kmer_library.h"
+#include <stdexcept>
+#include "naos_library.h"
 #include "cpas_debug.h"
-/*
-#include "htslib/sam.h"
-*/
+
+using namespace std;
 
 
-//parse alignments
-
-
-
-
-//detect candidates
-
-//curating candidates
-
-//output results
-
-
-
-int main(){
+int usage(){
+  cerr << "NAOS " << endl;
   return 0;
 }
+
+
+
+
+void countKmerFrequencies (
+    const char* FASTAFileName,
+    const char* SAMFileName,
+    uint KmerSize,
+    const bool outputInCSV,
+    const string binaryOutputFileName ///< empty() if --binary is not given
+    )
+{
+  fprintf(stderr, "\n===Parameters===\n");
+  fprintf(stderr, "Reference FASTA: %s\n", FASTAFileName);
+  fprintf(stderr, "Input SAM file : %s\n", SAMFileName);
+  fprintf(stderr, "K-mer size     : %d\n", KmerSize);
+  fprintf(stderr, "================\n");
+  fprintf(stderr, "\n");
+
+  fprintf(stderr, "Loading from the FASTA file ...\n");
+  const MultiFASTA multiFASTA = loadFromFASTA(FASTAFileName);
+  fprintf(stderr, "Done                           \n");
+  cerr << "reference record are as below\n---------" << endl;
+  for(auto itr = multiFASTA.begin(); itr != multiFASTA.end(); itr++) {
+    fprintf(stderr, "%s\n", itr->first.c_str());
+  }
+  cerr << "---------" << endl;
+  FastTSVParse ftp(SAMFileName);
+  if(!ftp) {
+    fprintf(stderr, "ERROR: Cannot open SAM file '%s'\n", SAMFileName);
+    exit(2);
+  }
+
+  SAMRecord record;
+  size_t recordCount = 0;
+  while(ftp.readNextLine()){
+    const char* line = ftp.c_str();
+    const bool isEmptyLine = line[0] == '\0';
+    if(isEmptyLine) continue;
+    const bool isCommentLine = line[0] == '@';
+    if(isCommentLine) continue;
+    if(!record.fill(ftp)) {
+      cerr << "SAM Parsing ERROR at line " << ftp.getLineNumber();
+      exit(2);
+    }
+    if(record.rname == "*") continue;
+    if(record.seq == "*") continue;
+
+    /*
+     * use only primary alignment and supplementary alignment and reverse compliment of them.
+     * sample has many supplimentary alignment.
+     */
+    if((record.flag & 2064) != record.flag) continue;
+
+    if(!multiFASTA.count(record.rname.c_str())) {
+      cerr << "SAM record says RNAME = '" << record.rname << "', but the reference genome does not have '" << record.rname << "'" << endl;
+      continue;
+      //exit(2);
+    }
+
+
+    // get aligned sequence at here
+    const CIGAROPS cops     = parseCIGARString(record.cigar);
+
+    BString refBS           = multiFASTA.at(record.rname);
+    BString queryBS         = String2BString(record.seq);
+    const int refStartPos   = record.pos;
+    const int queryStartPos = 0; // generateAlignmentSequencesFromCIGARAndSeqs() will manege first Softclip / Hardclip
+    BString ras, qas;
+    //generateAlignmentSequencesFromCIGARAndSeqs(refBS, queryBS, cops, refStartPos, queryStartPos, ras, qas);
+
+    // if record flag has revcomp flag, modify aligned reference sequence and read sequence.
+    if((record.flag & 16) != 16){
+      revCompBString(ras);
+      revCompBString(qas);
+    }
+    ++recordCount;
+    if(recordCount % 100 == 0) {
+      cerr << recordCount << " processed\r" << flush;
+    }
+  }
+  cerr << recordCount << " processed\n";
+  if(!binaryOutputFileName.empty()) {
+    //outputAsBinaryTable(binaryOutputFileName);
+  }
+}
+
+int main(int argc, char *argv[]){
+  GDB_On_SEGV g(argv[0]);
+  struct option longopts[] = {
+    { "csv"       , no_argument       , NULL , 'c' } ,
+    // { "delete" , optional_argument , NULL , 'd' } ,
+    { "kmer"      , required_argument , NULL , 'k' } ,
+    { "binary"    , required_argument , NULL , 'b' } ,
+    { 0           , 0                 , 0    , 0  }  ,
+  };
+
+  /// PARAMETERS ///
+
+  //int kmer_size = 1;
+  //bool output_in_csv = false;
+  //string binary_output_file_name;
+
+  //////////////////
+
+  /*
+     int opt;
+     int longindex;
+     while ((opt = getopt_long(argc, argv, "k:", longopts, &longindex)) != -1) {
+     switch (opt) {
+     case 'b':
+     binary_output_file_name = optarg;
+     break;
+     case 'c':
+     output_in_csv = true;
+     break;
+     case 'k':
+     kmer_size = atoi(optarg);
+     if(kmer_size < 1 || 10 < kmer_size) {
+     error("kmer size must be 1-10");
+     }
+     break;
+     default:
+     MYASSERT_NEVERREACH();
+     }
+     }
+     const int NUM_REQUIRED_ARGUMENTS = 2;
+     if(optind + NUM_REQUIRED_ARGUMENTS != argc) {
+     printUsageAndExit();
+     }
+     */
+  return 0;
+}
+
